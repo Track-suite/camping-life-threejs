ecd8473afd95ce86ec3567fb7218016b845986e0
Tracksuite <youngunner1998@gmail.com>
2022-03-02 16:33:29 +0530

    randomized trees

--- src/script.js
+++ src/script.js
@@ -90,7 +90,7 @@ const gui = new dat.GUI();
      updateAllMaterials();
    });
  }
- let model1, model2, model3, model4, model5, model6, model7;
+ let model1, model2, model3, model4, model5, model6, model7, model8, model9;
 
  let p1 = loadModel("gltf/bedroll.glb").then((result) => {
    model1 = result.scene.children[0];
@@ -108,26 +108,37 @@ const gui = new dat.GUI();
  let p5 = loadModel("gltf/tent.glb").then((result) => {
    model5 = result.scene.children[0];
  });
- let p6 = loadModel("gltf/resourceWood.glb").then((result) => {
+ let p6 = loadModel("gltf/structureCloth.glb").then((result) => {
    model6 = result.scene.children[0];
  });
 
+ let p7 = loadModel("gltf/rockB.glb").then((result) => {
+   model7 = result.scene.children[0];
+ });
+ let p8 = loadModel("gltf/rockC.glb").then((result) => {
+   model8 = result.scene.children[0];
+ });
+
  //if all Promises resolved
- Promise.all([p1, p2, p3, p4, p5, p6]).then(() => {
+ Promise.all([p1, p2, p3, p4, p5, p6, p7, p8]).then(() => {
    //do something to the model
    model1.position.set(0, 0.5, 0);
    model2.position.set(0, 0, -1);
    model3.position.set(0, 0, 0);
    model4.position.set(0, 0, -6);
    model5.position.set(-4, 0, 0);
-   model6.position.set(2, 0, -1);
+   model6.position.set(2, 0, -2.5);
+   model7.position.set(6, 0, -7);
+   model8.position.set(-6, 0, -7);
    /**
     *
     */
 
    //   setting the scale
-   model4.scale.set(4, 3, 4);
-   //   model5.scale.set(0.5, 0.5, 0.5);
+   model4.scale.set(5, 5, 5);
+   model7.scale.set(3, 5, 2);
+   model8.scale.set(3, 3, 4);
+
    //add model to the scene
    scene.add(model1);
    scene.add(model2);
@@ -135,6 +146,8 @@ const gui = new dat.GUI();
    scene.add(model4);
    scene.add(model5);
    scene.add(model6);
+   scene.add(model7);
+   scene.add(model8);
    //   scene.add(model5);
 
    //continue the process
@@ -164,7 +177,7 @@ const gui = new dat.GUI();
  // tree
  for (let i = 0; i < 20; i++) {
    const angle = Math.random() * Math.PI * 2; // Random angle
-   const radius = 5 + Math.random() * 6; // Random radius
+   const radius = 6 + Math.random(); // Random radius
    const x = Math.cos(angle) * radius; // Get the x position using cosinus
    const z = Math.sin(angle) * radius; // Get the z position using sinus
 
@@ -246,9 +259,9 @@ const gui = new dat.GUI();
   * Lights
   */
  // Ambient light
- //  const ambientLight = new THREE.AmbientLight("#ffffff", 0.5);
- //  gui.add(ambientLight, "intensity").min(0).max(1).step(0.001);
- //  scene.add(ambientLight);
+ const ambientLight = new THREE.AmbientLight("#ffffff", 0.5);
+ gui.add(ambientLight, "intensity").min(0).max(1).step(0.001);
+ scene.add(ambientLight);
 
  // Directional light
  const moonLight = new THREE.DirectionalLight("#ffffff", 1.5);
